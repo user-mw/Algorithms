9b939cf5be9a441b5554aa0defd9dcfa91beaf85
Titan-mw <myprbus@gmail.com>
2018-09-02 20:11:50 +0200

    Generic quick sorting added.

--- app/src/main/java/com/workout/algorithms/algorithms/QuickSorter.java
+++ app/src/main/java/com/workout/algorithms/algorithms/QuickSorter.java
@@ -1,7 +1,53 @@
 package com.workout.algorithms.algorithms;
 
-public class QuickSorter {
+public class QuickSorter<T extends Comparable> {
     private long[] mUsingArray;
+    private T[] mGenericArray;
+
+    public QuickSorter(T[] newArray) {
+        mGenericArray = newArray;
+    }
+
+    public void performSortingForAll(int left, int right) {
+        if(right - left <= 0) {
+            return;
+        } else {
+            T pivot = mGenericArray[right];
+
+            int partition = partitionItForAll(left, right, pivot);
+            performSortingForAll(left, partition - 1);
+            performSortingForAll(partition + 1, right);
+        }
+    }
+
+    private int partitionItForAll(int left, int right, T pivot) {
+        int leftPtr = left;
+        int rightPtr = right - 1;
+
+        while(true) {
+            while(mGenericArray[leftPtr].compareTo(pivot) < 0) {
+                leftPtr++;
+            }
+            while(rightPtr > 0 && mGenericArray[rightPtr].compareTo(pivot) > 0) {
+                rightPtr--;
+            }
+
+            if(leftPtr >= rightPtr) {
+                break;
+            } else {
+                swapForAll(leftPtr, rightPtr);
+            }
+        }
+
+        swapForAll(leftPtr, right);
+        return leftPtr;
+    }
+
+    private void swapForAll(int firstIndex, int secondIndex) {
+        T temp = mGenericArray[firstIndex];
+        mGenericArray[firstIndex] = mGenericArray[secondIndex];
+        mGenericArray[secondIndex] = temp;
+    }
 
     public QuickSorter(long[] newArray) { // not generic yet
         mUsingArray = newArray;
